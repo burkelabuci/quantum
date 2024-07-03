89ae1b1a58dfad11f8ef5ebf05b26d1ff7de4464
minghaoj <minghaoj@uci.edu>
2024-07-03 15:32:38 -0700

    test 3

--- noise.py
+++ noise.py
@@ -5,6 +5,7 @@
 
 #testing
 # test number 2
+# test number 3
 
 # This code sit the microwave frequency at sit_frequency
 # then extract the 10000 data points of voltage reading from Labjack T7 in a time period of t_collect,  then plot data point index vs Labjack Voltage (units)
