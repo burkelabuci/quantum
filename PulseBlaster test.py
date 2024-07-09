6c9cc08f8eb4d105ed8476b42657f899eb65f044
minghaoj <minghaoj@uci.edu>
2024-07-09 12:26:20 -0700

    some new comment added in PulseBlaster test.py

--- PulseBlaster test.py
+++ PulseBlaster test.py
@@ -1,13 +1,13 @@
-# T1_Decay_Subroutines.py
-# Subroutines to set pulse sequences for T1 decay of NV center measurement.
-# Peter Burke 7/3/2024
+# PulseBlaster test.py
+# Minghao Jiang 7/9/2024
 # Reference paper:
 # From reference paper: (aka "teaching paper")
 # Sewani, Vikas K., Hyma H. Vallabhapurapu, Yang Yang, Hannes R. Firgau, Chris Adambukulam, 
 # Brett C. Johnson, Jarryd J. Pla, and Arne Laucht. 
 # "Coherent control of NV− centers in diamond in a quantum teaching lab." American Journal of Physics 88, no. 12 (2020): 1156-1169.
-# 6/14/2024 Peter debugging Minghao's bug
-# Deletes Initialization pulses for demo purposes
+# This program produces Fig 3 on the teaching paper.
+# This program will need you to hard code the laser parameters tau_ref, tau_i, and tau_delay. It is usually used as a program to test if the pulseblaster
+# is working properly. If you wanna get data for T1_decay, go to the file T1_Decay.py
 
 import numpy as np
 import pandas as pd
