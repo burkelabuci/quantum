b5ffcc6033b69c14258ecea1c9ffbfa0b1099a9e
minghaoj <minghaoj@uci.edu>
2024-07-12 11:12:03 -0700

    sync start in software of pulseblaster

--- callpulseblasterhelperfunctions.py
+++ callpulseblasterhelperfunctions.py
@@ -9,6 +9,7 @@ import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 from labjack import ljm
 from pulsestreamer import PulseStreamer
+from pulsestreamer import *
 import keyboard
 import time 
 from T1_Decay_Subroutines import *
@@ -18,6 +19,11 @@ from T1_Decay_Subroutines import *
 PB_IPADDRESS= '169.254.8.2'
 
 ps = PulseStreamer(PB_IPADDRESS)
+
+ps.setTrigger(TriggerStart.SOFTWARE)
+
+
+
 
 # Create sequence object 
 #seq = ps.createSequence()
@@ -29,11 +35,20 @@ channel_number_ref=0
 channel_number_pulse=1
 delay_start_s=0.1e-3
 delay_stop_s=3e-3
-delay_number_of_points=10
+delay_number_of_points=1000
 
 
 
 do_it_all(channel_number_ref,channel_number_pulse,tau_ref_ns,tau_i_ns,number_of_cycles,delay_start_s,delay_stop_s,delay_number_of_points,ps)
+
+
+for i in range(1, 11):
+    print(i)
+    time.sleep(1)
+
+print("starting!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+ps.startNow()
+
 
 
 
