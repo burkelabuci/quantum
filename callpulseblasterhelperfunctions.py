d400ebb474007c4402dd52900123ae5f5a498b29
minghaoj <minghaoj@uci.edu>
2024-08-13 18:36:18 -0700

    time prediction for t2 and rabi added, and misc changes

--- callpulseblasterhelperfunctions.py
+++ callpulseblasterhelperfunctions.py
@@ -40,11 +40,11 @@ tau_delay_ns=1e-3*1e9
 
 # for Fig 4
 tau_laser_ns=5e-6*1e9
-tau_mw_ns=1e-6*1e9
+tau_mw_ns=5e-6*1e9
 tau_padding_ns=1e-6*1e9
-n_repeats=245 # 250  but with padding must be less
-tau_padding_before_mw_ns=100e-9*1e9
-tau_padding_after_mw_ns=100e-9*1e9
+n_repeats=200 # 250  but with padding must be less
+tau_padding_before_mw_ns=1000e-9*1e9
+tau_padding_after_mw_ns=1000e-9*1e9
 
 #create_fig3_teachingpaper_pulse_sequence(tau_ref_ns,tau_i_ns,tau_delay_ns,ps)
 #create_fig3_teachingpaper_pulse_sequence_no_init_pulse(tau_ref_ns,tau_i_ns,tau_delay_ns,ps)
